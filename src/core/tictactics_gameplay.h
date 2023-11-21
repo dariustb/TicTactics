c584c1b368a375c7330229510cb4aceb8e37faa4
Darius Brown <dariustb@tech-center.com>
2023-11-20 19:53:55 -0500

    Refactored core functionality

--- src/core/tictactics_gameplay.h
+++ src/core/tictactics_gameplay.h
@@ -2,4 +2,27 @@
 #ifndef INCLUDED_TICTACTICS_GAMEPLAY
 #define INCLUDED_TICTACTICS_GAMEPLAY
 
+#include <tictactics_board.h>
+#include <tictactics_player.h>
+#include <vector>
+
+class Game {
+    public:
+        Game();
+
+        std::vector<Player> Players();
+        Board               getBoard();
+
+        int addPlayer(Player player);
+        int play();
+        
+    private:
+        std::vector<Player> d_Players;
+        Board d_Board;
+
+        enum ReturnCodes {
+            e_SUCCESS,
+        };
+};
+
 #endif
--- src/core/tictactics_player.h
+++ src/core/tictactics_player.h
@@ -2,4 +2,18 @@
 #ifndef INCLUDED_TICTACTICS_PLAYER
 #define INCLUDED_TICTACTICS_PLAYER
 
+#include <string>
+
+class Player {
+    public:
+        Player(const std::string& name, const char& piece);
+
+        std::string name();
+        char        piece();
+
+    private:
+        std::string d_name;
+        char        d_piece;
+};
+
 #endif
